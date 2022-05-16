bf74f00014f08ad674816727be10db506267a12a
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-05-16 19:12:51 +0200

    own Exception and class Einwohner

---
+++ src/org/campus02/DataFileException.java
@@ -0,0 +1,8 @@
+package org.campus02;
+
+public class DataFileException extends Exception {
+
+    public DataFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
